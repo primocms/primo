4af75225e76c4e122643a6aee64f82a7e4c8b66d
Mateo Morris <accounts@mateomorris.com>
2020-07-24 19:04:44 -0400

    Outline columns when hovering

--- packages/primo-app/src/screens/Page/editor/Layout/Column.svelte
+++ packages/primo-app/src/screens/Page/editor/Layout/Column.svelte
@@ -7,3 +7,15 @@
 <div class="column {column.size}" id="column-{column.id}">
   <slot></slot>
 </div>
+
+<style>
+  .column {
+    outline: 1px solid transparent;
+    outline-color: transparent;
+    transition: 0.25s outline-color;
+  }
+  .column:hover {
+    outline-color: gainsboro;
+    transition: 0.25s outline-color;
+  }
+</style>
