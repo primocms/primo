48be1318d7e3d71b193a96f94811935b10cd0573
Mateo Morris <accounts@mateomorris.com>
2020-09-07 23:40:38 -0400

    symbol padding

--- src/views/modal/ComponentLibrary/ComponentContainer.svelte
+++ src/views/modal/ComponentLibrary/ComponentContainer.svelte
@@ -46,7 +46,7 @@
 
 </script>
 
-<article class="message component-wrapper" in:fade={{ delay: 250, duration: 200 }}>
+<article class="message component-wrapper mt-2" in:fade={{ delay: 250, duration: 200 }}>
   <form on:submit|preventDefault={changeTitle}>
     <input type="text" bind:this={titleInput} bind:value={title} on:blur={changeTitle} on:focus={() => editingTitle = true}/>
   </form>
