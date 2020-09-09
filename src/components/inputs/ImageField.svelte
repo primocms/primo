b7bdcf231dfd708350aa1e0857caf139afb1db5a
Mateo Morris <accounts@mateomorris.com>
2020-09-08 22:56:07 -0400

    hide image field label for now

--- src/components/inputs/ImageField.svelte
+++ src/components/inputs/ImageField.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <div class="flex flex-col p-4 shadow mb-2">
-  <span class="text-xl">{field.label}</span>
+  <!-- <span class="text-xl">{field.label}</span> -->
   <label class="flex items-center flex-1">
     <span class="font-semibold text-sm mr-2">Image URL</span>
     <input bind:value={field.value} on:input={() => dispatch('input', field.value)} class="bg-gray-200 p-2 flex-1" type="url" placeholder="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg">
