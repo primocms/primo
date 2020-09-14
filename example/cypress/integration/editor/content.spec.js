cbc93cf5aa2b9786f17160bba8bd570d00129eae
Mateo Morris <accounts@mateomorris.com>
2020-09-14 16:30:19 +0200

    basic content test

--- example/cypress/integration/editor/content.spec.js
+++ example/cypress/integration/editor/content.spec.js
@@ -1,25 +1,16 @@
 
 context('Editor', () => {
-  // beforeEach(() => {
-  //   cy.visit('http://test.localhost:5001')
-  // })
-
   describe('Creating content', () => {
 
-    it('logs into page', () => {
-      cy.visit('http://test.localhost:5001')
-      cy.get('#primo-symbol').click()
-      cy.get('#signin-github').click()
-    })
-
     it('types and saves text', () => {
+      cy.visit('http://cypress.localhost:5000')
       const randomString = Math.random() * 5;
-      const editor = cy.get('section .primo-content')
+      const editor = cy.get('.primo-content')
       editor.clear().type(randomString)
       cy.get('#save').click()
-      cy.wait(1000)
+      cy.wait(500)
       cy.reload()
-      editor.should('contain', randomString)
+      cy.get('.primo-content p').should('contain', randomString)
     })
 
   })
