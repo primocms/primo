4cf2c64fef1d0fc81cbf77b7a5ebed4765c48dde
Mateo Morris <accounts@mateomorris.com>
2020-09-15 14:58:51 +0200

    pass `--single` to sirv

--- example/rollup.config.js
+++ example/rollup.config.js
@@ -38,7 +38,7 @@ function serve() {
 	return {
 		writeBundle() {
 			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev', '--single'], {
 				stdio: ['ignore', 'inherit', 'inherit'],
 				shell: true
 			});
