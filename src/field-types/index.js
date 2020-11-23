80e0cb3da2ef4fda841dc5ba4d14796f78a99ee4
Mateo Morris <accounts@mateomorris.com>
2020-11-23 02:03:02 +0200

    export field types

--- src/field-types/index.js
+++ src/field-types/index.js
@@ -1,14 +1,27 @@
 import Number from './Number.svelte'
-import Slider from './Slider.svelte'
 import Content from './Content.svelte'
 import Switch from './Switch.svelte'
 import URL from './URL.svelte'
 
-export {
-  Number,
-  Content,
-  Switch,
-  URL
-}
-
-export default [ Number, Content, Switch, URL ]
+export default [ 
+  {
+    id: 'content',
+    label: 'Copy',
+    component: Content
+  },
+  {
+    id: 'number',
+    label: 'Number',
+    component: Number
+  },
+  {
+    id: 'switch',
+    label: 'Switch',
+    component: Switch
+  },
+  {
+    id: 'url',
+    label: 'URL',
+    component: URL
+  }
+]
