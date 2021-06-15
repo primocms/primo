9b1e95e77f008b239ec88eb37a517d738197b55f
mateomorris <accounts@mateomorris.com>
2021-06-14 22:05:27 -0400

    format

--- src/components/FieldTypes/RepeaterField.svelte
+++ src/components/FieldTypes/RepeaterField.svelte
@@ -1,61 +1,75 @@
 <script>
-  import _ from 'lodash'
-  import pluralize from 'pluralize'
-  import {fade} from 'svelte/transition'
-  import {createEventDispatcher} from 'svelte'
-  const dispatch = createEventDispatcher() 
+  import _ from "lodash";
+  import pluralize from "pluralize";
+  import { fade } from "svelte/transition";
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
 
-  import {createUniqueID} from '../../utilities'
-  import {Card} from '../misc'
-  import { EditField, GenericField, ImageField} from '../inputs'
-  import fieldTypes from '../../stores/app/fieldTypes'
+  import { createUniqueID } from "../../utilities";
+  import { Card } from "../misc";
+  import { EditField, GenericField, ImageField } from "../inputs";
+  import fieldTypes from "../../stores/app/fieldTypes";
 
-  export let field
+  console.log(pluralize);
+
+  export let field;
 
   function addRepeaterItem() {
-    const keys = field.fields.map(f => f.key)
-    fieldValues = [
-      ...fieldValues,
-      createSubfield()
-    ]
-    dispatch('input')
+    const keys = field.fields.map((f) => f.key);
+    fieldValues = [...fieldValues, createSubfield()];
+    dispatch("input");
   }
 
   function removeRepeaterItem(itemIndex) {
-    fieldValues = fieldValues.filter((_, i) => i !== itemIndex)
-    onInput()
+    fieldValues = fieldValues.filter((_, i) => i !== itemIndex);
+    onInput();
   }
 
   function moveRepeaterItem(indexOfItem, direction) {
-    const item = fieldValues[indexOfItem]
-    const withoutItem = fieldValues.filter((_, i) => i !== indexOfItem)
-    if (direction === 'up') {
-      fieldValues = [...withoutItem.slice(0,indexOfItem-1), item, ...withoutItem.slice(indexOfItem-1)];
-    } else if (direction === 'down') {
-      fieldValues = [...withoutItem.slice(0, indexOfItem+1), item, ...withoutItem.slice(indexOfItem+1)];
+    const item = fieldValues[indexOfItem];
+    const withoutItem = fieldValues.filter((_, i) => i !== indexOfItem);
+    if (direction === "up") {
+      fieldValues = [
+        ...withoutItem.slice(0, indexOfItem - 1),
+        item,
+        ...withoutItem.slice(indexOfItem - 1),
+      ];
+    } else if (direction === "down") {
+      fieldValues = [
+        ...withoutItem.slice(0, indexOfItem + 1),
+        item,
+        ...withoutItem.slice(indexOfItem + 1),
+      ];
     } else {
-      console.error('Direction must be up or down')
+      console.error("Direction must be up or down");
     }
   }
 
   function createSubfield() {
-    return field.fields.map(subfield => ({
+    return field.fields.map((subfield) => ({
       ...subfield,
       id: createUniqueID(),
-      value: ''
-    }))
+      value: "",
+    }));
   }
 
-  let fieldValues = Array.isArray(field.value) ? field.value.map(value => [
-    ...field.fields.map(subfield => ({
-      ...subfield,
-      value: value[subfield.key]
-    }))
-  ]) : []
+  let fieldValues = Array.isArray(field.value)
+    ? field.value.map((value) => [
+        ...field.fields.map((subfield) => ({
+          ...subfield,
+          value: value[subfield.key],
+        })),
+      ])
+    : [];
 
   function onInput() {
-    field.value = fieldValues.map(fieldValue => fieldValue.reduce((obj, item) => Object.assign(obj, { [item.key]: item.value }), {}))
-    dispatch('input')
+    field.value = fieldValues.map((fieldValue) =>
+      fieldValue.reduce(
+        (obj, item) => Object.assign(obj, { [item.key]: item.value }),
+        {}
+      )
+    );
+    dispatch("input");
   }
 
 </script>
@@ -63,60 +77,84 @@
 <Card variants="pb-4" id="repeater-{field.key}">
   <header class="w-full py-1 font-bold text-sm">{field.label}</header>
   <div class="grid grid-cols-2 gap-2">
-    {#each fieldValues as fieldValue, i} 
-      <div class="repeater-item" id="repeater-{field.key}-{i}" in:fade={{duration:100}}>
-        <div class="absolute top-0 right-0 py-1 px-2 text-gray-200 bg-gray-900 z-10 rounded">
+    {#each fieldValues as fieldValue, i}
+      <div
+        class="repeater-item"
+        id="repeater-{field.key}-{i}"
+        in:fade={{ duration: 100 }}
+      >
+        <div
+          class="absolute top-0 right-0 py-1 px-2 text-gray-200 bg-gray-900 z-10 rounded"
+        >
           {#if i !== 0}
-            <button title="Move {field.label} up" on:click={() => moveRepeaterItem(i, 'up')}>
-              <i class="fas fa-arrow-up"></i>
+            <button
+              title="Move {field.label} up"
+              on:click={() => moveRepeaterItem(i, 'up')}
+            >
+              <i class="fas fa-arrow-up" />
             </button>
           {/if}
-          {#if i !== fieldValues.length-1}
-            <button class="mr-2" title="Move {field.label} down" on:click={() => moveRepeaterItem(i, 'down')}>
-              <i class="fas fa-arrow-down"></i>
+          {#if i !== fieldValues.length - 1}
+            <button
+              class="mr-2"
+              title="Move {field.label} down"
+              on:click={() => moveRepeaterItem(i, 'down')}
+            >
+              <i class="fas fa-arrow-down" />
             </button>
           {/if}
-          <button class="text-red-400 hover:text-red-500" title="Delete {field.label} item" on:click={() => removeRepeaterItem(i)}>
-            <i class="fas fa-trash"></i>
+          <button
+            class="text-red-400 hover:text-red-500"
+            title="Delete {field.label} item"
+            on:click={() => removeRepeaterItem(i)}
+          >
+            <i class="fas fa-trash" />
           </button>
         </div>
         {#each fieldValue as subfield}
-          <div class="repeater-item-field" id="repeater-{field.key}-{i}-{subfield.key}">
-            <svelte:component this={_.find($fieldTypes, ['id', subfield.type]).component} field={subfield} on:input={onInput} />
+          <div
+            class="repeater-item-field"
+            id="repeater-{field.key}-{i}-{subfield.key}"
+          >
+            <svelte:component
+              this={_.find($fieldTypes, ['id', subfield.type]).component}
+              field={subfield}
+              on:input={onInput}
+            />
           </div>
         {/each}
       </div>
     {/each}
     <div class="p-2 bg-gray-900">
       <button class="field-button" on:click={() => addRepeaterItem()}>
-        <i class="fas fa-plus mr-1"></i>
+        <i class="fas fa-plus mr-1" />
         <span>Add {pluralize.singular(field.label)}</span>
       </button>
     </div>
   </div>
 </Card>
 
-
 <style>
   .repeater-item {
     @apply p-4 bg-gray-900 flex flex-col relative border-2 border-primored;
   }
   .repeater-item:last-of-type {
-      @apply mb-0;
-    }
+    @apply mb-0;
+  }
   .repeater-item-field {
     @apply mb-2;
   }
   .repeater-item-field:not(:first-child) {
-      @apply pt-0;
-    }
+    @apply pt-0;
+  }
   .field-button {
     @apply w-full bg-gray-800 text-gray-300 py-2 rounded font-medium transition-colors duration-200;
   }
   .field-button:hover {
-      @apply bg-gray-900;
-    }
-    .field-button[disabled] {
-      @apply bg-gray-500 cursor-not-allowed;
-    }
+    @apply bg-gray-900;
+  }
+  .field-button[disabled] {
+    @apply bg-gray-500 cursor-not-allowed;
+  }
+
 </style>
--- src/views/editor/Layout/ComponentPicker/ComponentPicker.svelte
+++ src/views/editor/Layout/ComponentPicker/ComponentPicker.svelte
@@ -1,80 +1,80 @@
 <script>
-  import {fade} from 'svelte/transition'
-  import { createEventDispatcher, onMount } from 'svelte'
-  import _ from 'lodash'
-  import Container from './ComponentPickerContainer.svelte'
+  import { fade } from "svelte/transition";
+  import { createEventDispatcher, onMount } from "svelte";
+  import _ from "lodash";
+  import Container from "./ComponentPickerContainer.svelte";
 
-  import { symbols } from '../../../../stores/data/draft'
-  import { createUniqueID } from '../../../../utilities'
+  import { symbols } from "../../../../stores/data/draft";
+  import { createUniqueID } from "../../../../utilities";
 
-  const dispatch = createEventDispatcher()
+  const dispatch = createEventDispatcher();
 
-  function getID(symbol) {
-    return symbol.id + symbol.value.html + symbol.value.css
-  }
+  const componentID = (component) =>
+    component.id + component.value.html + component.value.css;
 
   function createInstance(symbol) {
-    const instanceID = createUniqueID()
+    const instanceID = createUniqueID();
     return {
-      type: 'component',
+      type: "component",
       id: instanceID,
       symbolID: symbol.id,
       value: {
-        fields: symbol.value.fields
+        fields: symbol.value.fields,
       },
-    }
+    };
   }
 
-  let mounted = false
+  let mounted = false;
   onMount(() => {
-    mounted = true
-  })
+    mounted = true;
+  });
 
-  let element
+  let element;
 
   onMount(() => {
     element.scrollIntoView({
-      behavior: 'smooth',
-      block: 'center',
-      inline: 'center'
+      behavior: "smooth",
+      block: "center",
+      inline: "center",
     });
-  })
+  });
 
 </script>
 
-<main in:fade={{duration:100}} bind:this={element}>
-  <div class="flex justify-between items-center">
-    <button class="close text-gray-100 hover:text-primored transition-colors duration-100" on:click={() => dispatch('remove')} type="button" xyz="small" aria-label="Close modal">
-      <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+<main in:fade={{ duration: 100 }} bind:this={element}>
+  <div class="buttons">
+    <button
+      class="close"
+      on:click={() => dispatch('remove')}
+      type="button"
+      xyz="small"
+      aria-label="Close modal"
+    >
+      <svg stroke="currentColor" fill="none" viewBox="0 0 24 24">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M6 18L18 6M6 6l12 12"
+        />
       </svg>
     </button>
     <button class="button" on:click={() => dispatch('manage')}>
-      <i class="fas fa-code mr-1"></i>
+      <i class="fas fa-code" />
       <span>Manage Components</span>
     </button>
   </div>
   {#if mounted}
-    <ul
-      class="grid md:grid-cols-3 lg:grid-cols-4 gap-4 shadow-inner"
-      xyz="fade stagger stagger-2"
-    >
-      {#each $symbols as symbol (getID(symbol))}
+    <ul class="components" xyz="fade stagger stagger-2">
+      {#each $symbols as symbol (componentID(symbol))}
         <li class="xyz-in">
           <Container
             titleEditable
             {symbol}
             loadPreview={mounted}
-            buttons={[
-              { 
-                onclick: () => {
-                  dispatch('select', createInstance(symbol))
-                },
-                highlight: true,
-                label: 'Select',
-                svg: `<svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd"></path></svg>`,
-              },
-            ]}
+            buttons={[{ onclick: () => {
+                  dispatch('select', createInstance(symbol));
+                }, highlight: true, label: 'Select', svg: `<svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd"></path></svg>` }]}
           />
         </li>
       {/each}
@@ -86,11 +86,18 @@
   main {
     padding: 2.5rem;
     overflow-x: scroll;
-    @apply bg-codeblack border-4 border-primored;
     height: 75vh;
+    background-color: rgba(30, 30, 30, var(--tw-bg-opacity));
+    border-width: 4px;
+    border-color: rgba(248, 68, 73, var(--tw-border-opacity));
+  }
+  .buttons {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
   .button {
-    background: rgb(248,68,73);
+    background: rgb(248, 68, 73);
     padding: 0.5rem 1rem;
     margin-bottom: 1rem;
     border-radius: 0.25rem;
@@ -99,11 +106,45 @@
     font-size: 0.75rem;
     transition: color 0.1s;
 
+    i {
+      margin-right: 0.25rem;
+    }
+
+    svg {
+      height: 1.5rem;
+      width: 1.5rem;
+    }
+
     &:hover {
-      @apply bg-red-600;
+      background-color: rgba(220, 38, 38, var(--tw-bg-opacity));
     }
   }
   button.close {
-    @apply transform -translate-y-8 -translate-x-4;
+    transform: translateX(-1rem) translateY(-2rem);
+    color: rgba(245, 245, 245, var(--tw-text-opacity));
+
+    &:hover {
+      color: rgba(248, 68, 73, var(--tw-text-opacity));
+    }
   }
+
+  .components {
+    display: grid;
+    gap: 1rem;
+    box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000),
+      var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+  }
+
+  @media (min-width: 768px) {
+    .components {
+      grid-template-columns: repeat(3, minmax(0, 1fr));
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .components {
+      grid-template-columns: repeat(4, minmax(0, 1fr));
+    }
+  }
+
 </style>
