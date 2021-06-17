03408a79d783451565d02e3b762dd140093ef4ba
mateomorris <accounts@mateomorris.com>
2021-06-17 13:59:18 -0400

    Make TextInput accept dynamic type

--- src/components/inputs/TextInput.svelte
+++ src/components/inputs/TextInput.svelte
@@ -1,28 +1,36 @@
 <script>
-  import {createEventDispatcher} from 'svelte'
+  import { createEventDispatcher } from 'svelte';
   const dispatch = createEventDispatcher();
 
-  export let id
-  export let label
-  export let prefix = ''
-  export let value
-  export let placeholder = ''
-  export let variants = ''
-  export let autofocus = false
+  export let id;
+  export let label;
+  export let prefix = '';
+  export let value;
+  export let placeholder = '';
+  export let variants = '';
+  export let autofocus = false;
+  export let type = 'text';
 
   // Note: Svelte seems to have some issues with two-way binding, so if this is acting up it's probably that
+
 </script>
 
 <!-- svelte-ignore a11y-label-has-associated-control -->
 <label class="flex flex-col items-start text-gray-200 {variants}" {id}>
-  {#if label}
-     <span class="mb-1 text-gray-200 font-semibold">{label}</span>
-  {/if}
+  {#if label}<span class="mb-1 text-gray-200 font-semibold">{label}</span>{/if}
   <div class="flex items-normal w-full">
     {#if prefix}
       <span class="font-medium flex items-center mr-4">{prefix}</span>
     {/if}
-    <input {autofocus} bind:value type="text" {placeholder} class="inline-block flex-1" on:input={() => dispatch('input', value)} />
+    <input
+      {autofocus}
+      {value}
+      {type}
+      {placeholder}
+      class="inline-block flex-1"
+      on:input={({ target }) => {
+        value = target.value;
+      }} />
   </div>
 </label>
 
@@ -30,4 +38,5 @@
   input {
     @apply bg-gray-800 text-gray-100 font-medium rounded-sm py-2 px-3 flex-1;
   }
+
 </style>
