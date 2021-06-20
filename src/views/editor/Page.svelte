3114e6782eb2cadaace1157aaa174476df5fd7ad
mateomorris <accounts@mateomorris.com>
2021-06-19 21:03:29 -0400

    include site html in doc head

--- src/views/editor/Page.svelte
+++ src/views/editor/Page.svelte
@@ -1,30 +1,34 @@
 <script>
-  import _ from 'lodash'
-  import Editor from './Editor.svelte'
-  import { wrapInStyleTags } from '../../utils'
+  import _ from 'lodash';
+  import Editor from './Editor.svelte';
+  import { wrapInStyleTags } from '../../utils';
 
-  import tailwind from '../../stores/data/tailwind'
-  import {styles as siteStyles} from '../../stores/data/draft'
+  import tailwind from '../../stores/data/tailwind';
   import {
-    id, 
-    styles as pageStyles, 
-    wrapper as pageWrapper
-  } from '../../stores/app/activePage'
-  import {unsaved} from '../../stores/app/misc'
-  import site from '../../stores/data/site'
+    styles as siteStyles,
+    wrapper as siteWrapper,
+  } from '../../stores/data/draft';
+  import {
+    id,
+    styles as pageStyles,
+    wrapper as pageWrapper,
+  } from '../../stores/app/activePage';
+  import { unsaved } from '../../stores/app/misc';
+  import site from '../../stores/data/site';
 
   function savePage() {
-    $unsaved = false
-    site.save()
+    $unsaved = false;
+    site.save();
   }
 
 </script>
 
 <svelte:head>
   {@html wrapInStyleTags($tailwind, 'tailwind')}
+  {@html $siteWrapper.head.final}
   {@html $pageWrapper.head.final}
   {@html wrapInStyleTags($siteStyles.final, 'site-styles')}
-  {@html wrapInStyleTags($pageStyles.final, "page-styles")}
+  {@html wrapInStyleTags($pageStyles.final, 'page-styles')}
 </svelte:head>
 
 <Editor on:change on:save={savePage} on:build on:signOut />
