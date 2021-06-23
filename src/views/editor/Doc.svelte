9696a933a1e849f413153980d95c0c524e115c09
mateomorris <accounts@mateomorris.com>
2021-06-23 13:49:58 -0400

    Address issue with group subfields

--- src/views/editor/Doc.svelte
+++ src/views/editor/Doc.svelte
@@ -1,73 +1,83 @@
 <script>
   import { onMount, tick } from 'svelte';
-  import {find,unionBy} from 'lodash'
-  import Block from './Layout/Block.svelte'
-  import { pages, wrapper as siteWrapper, symbols, fields as siteFields } from '../../stores/data/draft'
-  import { id, wrapper as pageWrapper, content, fields as pageFields } from '../../stores/app/activePage'
+  import { find, unionBy } from 'lodash';
+  import Block from './Layout/Block.svelte';
+  import {
+    pages,
+    wrapper as siteWrapper,
+    symbols,
+    fields as siteFields,
+  } from '../../stores/data/draft';
+  import {
+    id,
+    wrapper as pageWrapper,
+    content,
+    fields as pageFields,
+  } from '../../stores/app/activePage';
 
-  $: pageExists = findPage($id, $pages)
+  $: pageExists = findPage($id, $pages);
   function findPage(id, pages) {
-    const [ root ] = id.split('/')
-    const rootPage = find(pages, ['id', root])
-    const childPage = rootPage ? find(rootPage?.pages, ['id', id]) : null
-    return childPage || rootPage
+    const [root] = id.split('/');
+    const rootPage = find(pages, ['id', root]);
+    const childPage = rootPage ? find(rootPage?.pages, ['id', id]) : null;
+    return childPage || rootPage;
   }
 
   function hydrateInstance(block, symbols) {
-    const symbol = find(symbols, ['id', block.symbolID])
+    const symbol = find(symbols, ['id', block.symbolID]);
     // overwrite the symbol field values
     return {
       ...block,
       value: {
         ...symbol.value,
-        fields: symbol.value.fields.map(field => {
-          const originalField = find(block.value.fields, ['id', field.id]) || find(symbol.value.fields, ['id', field.id]) || { value: '' }
+        fields: symbol.value.fields.map((field) => {
+          const originalField = find(block.value.fields, ['id', field.id]) ||
+            find(symbol.value.fields, ['id', field.id]) || { value: '' };
           return {
             ...field,
-            fields: field.type === 'repeater' ? field.fields : originalField.fields,
-            value: originalField.value
-          }
-        })
-      }
-    }
+            fields: ['repeater', 'group'].includes(field.type)
+              ? field.fields
+              : originalField.fields,
+            value: originalField.value,
+          };
+        }),
+      },
+    };
   }
 
   let element;
 
   // Disable the links on the page that don't navigate to a page within primo
-  // TODO: prevent navigating away from site 
+  // TODO: prevent navigating away from site
   // prevent navigating to pages that don't exist
   async function disableLinks(_) {
-    if (!element) return
+    if (!element) return;
     setTimeout(() => {
-      element.querySelectorAll('a').forEach(link => {
+      element.querySelectorAll('a').forEach((link) => {
         if (window.location.host !== link.host) {
           link.setAttribute('data-tinro-ignore', '');
           link.onclick = (e) => {
-            e.preventDefault()
-            window.open(link.href, '_blank')
-          }
+            e.preventDefault();
+            window.open(link.href, '_blank');
+          };
         }
-      })
-    }, 100)
+      });
+    }, 100);
   }
-  $: disableLinks($content)
+  $: disableLinks($content);
 
 </script>
 
-<div bind:this={element} class="primo-page" style="border-top: 48px solid rgb(20,20,20)">
+<div
+  bind:this={element}
+  class="primo-page"
+  style="border-top: 48px solid rgb(20,20,20)">
   {#if pageExists}
     {#each $content as block, i (block.id)}
       {#if block.symbolID}
-        <Block block={
-          hydrateInstance(
-            block,
-            $symbols,
-            $pageFields,
-            $siteFields
-          )} 
-          {i} 
-        />
+        <Block
+          block={hydrateInstance(block, $symbols, $pageFields, $siteFields)}
+          {i} />
       {:else}
         <Block {block} {i} />
       {/if}
