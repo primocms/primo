a499fb23f29eb0bcea60f09686428af6d70bf8d7
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2021-06-28 19:49:10 +0200

    convert fifth component

--- src/views/editor/Layout/CopyButton.svelte
+++ src/views/editor/Layout/CopyButton.svelte
@@ -12,23 +12,48 @@
 
 <style>
   button {
-    @apply flex items-center justify-center py-2 px-3 ring-1 ring-transparent transition-colors duration-100;
+    @apply ring-1;
+    --tw-ring-color: transparent;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    justify-content: center;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    padding-left: 0.75rem;
+    padding-right: 0.75rem;
+    transition-duration: 100ms;
+    transition-property: background-color, border-color, color, fill, stroke;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 150ms; /* do we need this */
+    /* --tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color);
+    --tw-ring-shadow: var(--tw-ring-inset) 0 0 0 calc(1px + var(--tw-ring-offset-width)) var(--tw-ring-color);
+    box-shadow: var(--tw-ring-offset-shadow), var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000); */
 
     i {
-      @apply opacity-100 transition-opacity duration-100;
+      opacity: 1;
+      transition-property: opacity;
+      transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+      transition-duration: 150ms; /* do we need this */
+      transition-duration: 100ms;
     }
   }
   button:hover {
-    @apply bg-primored text-gray-100;
+
+    background-color: rgba(248, 68, 73, var(--tw-bg-opacity));
+    color: rgba(245, 245, 245, var(--tw-text-opacity));
 
     /* i {
       @apply opacity-0;
+      opacity: 0;
     }
     span {
       @apply opacity-100;
+      opacity: 1;
     } */
   }
   .active {
-    @apply bg-primored text-white;
+    background-color: rgba(248, 68, 73, var(--tw-bg-opacity));
+    color: rgba(255, 255, 255, var(--tw-text-opacity));
   }
 </style>
