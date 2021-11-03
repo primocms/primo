0af1454b270dd4f3f36a75b4def4bac3d9ac543e
mateomorris <accounts@mateomorris.com>
2021-11-03 14:48:57 +0200

    add fontawesome link

--- src/Primo.svelte
+++ src/Primo.svelte
@@ -110,6 +110,10 @@
   <svelte:component this={activeModal} {...$modal.componentProps} />
 </Modal>
 
+<svelte:head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</svelte:head>
+
 <style>
   :global(:root) {
     --primo-color-primored: rgb(248, 68, 73);
