405bdb187f4943587fffa1471612f78d26df55f0
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2022-01-22 13:40:12 +0200

    add: Editor tests

---
+++ tests/cypress/integration/editorFields.test.js
@@ -0,0 +1,16 @@
+describe('Dashboard', () => {
+
+    it('visit the page', () => {
+        cy.visit('http://localhost:3000/demo')
+    })
+
+    it('Modify page fields', () => {
+        
+    });
+
+    it("Modify Site fields", () => {
+        
+    });
+});
+
+
