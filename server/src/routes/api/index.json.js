c5ae471db420e6d3a9c961dc3ab7bf94f4d44859
Mateo Morris <accounts@mateomorris.com>
2022-02-17 14:02:20 +0200

    try again

--- server/src/routes/api/index.json.js
+++ server/src/routes/api/index.json.js
@@ -1,6 +1,13 @@
 import { authorizeRequest } from './_auth'
 
 export async function get(event) {
+  return {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    status: 201,
+    body: 'yes'
+  }
   return await authorizeRequest(event, () => ({
     body: {
       success: true
