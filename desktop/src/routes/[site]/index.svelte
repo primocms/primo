259a8571f77af82234ad28b28a41abbfaf43caa8
Mateo Morris <accounts@mateomorris.com>
2022-02-24 17:36:35 +0200

    undo page creation bug workaround

--- desktop/src/routes/[site]/index.svelte
+++ desktop/src/routes/[site]/index.svelte
@@ -1,11 +1,1 @@
-<script context="module">
-  // this is a workaround for a sveltekit bug where newly-created links weren't being handled correctly (i.e. reloading the page)
-  // a few seconds after mounting, causing an issue where newly-created pages would reload the site (and lose progress) if clicked immediately after creation
-	/** @type {import('@sveltejs/kit').Load} */
-	export async function load({ params }) {
-		return {
-			redirect: `/${params.site}/index`, // load [site]/[...page].svelte
-      status: 300
-		};
-	}
-</script>
+<!-- intentionally left blank -->
