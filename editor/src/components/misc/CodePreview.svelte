ddef6e60aeda9f8f689226514278adc9fce607f4
Mateo Morris <accounts@mateomorris.com>
2022-04-27 00:07:56 +0200

    fix import error

--- editor/src/components/misc/CodePreview.svelte
+++ editor/src/components/misc/CodePreview.svelte
@@ -2,7 +2,7 @@
   import { onMount, onDestroy } from 'svelte';
   import { slide } from 'svelte/transition';
   import { iframePreview } from './misc';
-  import {locale, highlightedElement } from '../../stores/app/misc'
+  import {locale} from '../../stores/app/misc'
   import JSONTree from 'svelte-json-tree';
   import Icon from '@iconify/svelte'
 
