304a881c915b3465d06ca28bc47ef87afffd4cfc
Mateo Morris <accounts@mateomorris.com>
2023-03-31 23:09:23 -0400

    feature: show page preview with ?preview

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,7 +1,22 @@
 import '$lib/supabase'
+import supabaseAdmin from './supabase/admin';
 
 export const handle = async ({ resolve, event }) => {
   const response = await resolve(event);
+
+  const is_preview = event.url.searchParams.has('preview')
+  if (is_preview) {
+    const {data,error} = await supabaseAdmin.from('pages').select('preview, site!inner(*)').match({
+      'site.url': event.params.site,
+      url: event.params.page || 'index'
+    }).single()
+    return new Response(data ? data.preview : 'none found', {
+      headers: {  
+        'Content-Type': 'text/html;charset=UTF-8',
+        'Access-Control-Allow-Origin': '*',
+      },
+    })
+  }
 
   if(event.request.method === 'OPTIONS') {
     return new Response(null, {
