084090bd3e62d945a2c1d90a202d3c2da5720789
Mateo Morris <39444813+mateomorris@users.noreply.github.com>
2023-08-22 21:54:54 -0400

    improve module imports #264 (#338)

--- src/compiler/workers/rollup.worker.js
+++ src/compiler/workers/rollup.worker.js
@@ -1,207 +1,363 @@
-import { rollup } from "../lib/rollup-browser";
+import { rollup } from '@rollup/browser'
 import svelteWorker from './svelte.worker?worker'
-import PromiseWorker from 'promise-worker';
+import PromiseWorker from 'promise-worker'
 import registerPromiseWorker from 'promise-worker/register'
+import * as resolve from 'resolve.exports'
+import commonjs from './plugins/commonjs'
+import json from './plugins/json'
+import glsl from './plugins/glsl'
 
-const sveltePromiseWorker = new PromiseWorker(new svelteWorker());
+const sveltePromiseWorker = new PromiseWorker(new svelteWorker())
 
-// Based on https://github.com/pngwn/REPLicant
+// Based on https://github.com/pngwn/REPLicant & the Svelte REPL package (https://github.com/sveltejs/sites/tree/master/packages/repl)
 
-const CDN_URL = "https://cdn.jsdelivr.net/npm";
+const CDN_URL = 'https://cdn.jsdelivr.net/npm' // or 'https://unpkg.com'
 
-const cached_packages = new Map()
+registerPromiseWorker(rollup_worker)
+async function rollup_worker({ component, hydrated, buildStatic = true, format = 'esm' }) {
+	const final = {
+		ssr: '',
+		dom: '',
+		error: null
+	}
 
-async function fetch_package(url) {
-    if (cached_packages.has(url)) {
-        return cached_packages.get(url)
-    } else {
-        const pck = await (await fetch(url)).text()
-        cached_packages.set(url, pck)
-        return pck;
-    }
-}
+	const component_lookup = new Map()
 
-registerPromiseWorker(async function ({ component, hydrated, buildStatic = true, format = 'esm' }) {
+	const Svelte_Component_Code = (component, is_single = true) => {
+		let { html, css, js, data } = component
+		const data_as_variables = is_single
+			? Object.entries(data)
+					.filter((field) => field[0])
+					.map((field) => `export let ${field[0]};`)
+					.join(`\n`)
+			: Object.entries(data)
+					.filter((field) => field[0])
+					.map((field) => `let ${field[0]} = props['${field[0]}'];`)
+					.join(`\n`)
 
-    const final = {
-        ssr: '',
-        dom: '',
-        error: null
-    }
+		// Move <svelte:window> outside the encompassing <div> to prevent 'can't nest' error
+		if (html.includes('<svelte:window')) {
+			let [svelteWindowTag] = html.match(/<svelte:window(.*?)\/>/)
+			html = html.replace(svelteWindowTag, '')
+			html = html + svelteWindowTag
+		}
 
-    const component_lookup = new Map();
-
-    const Component_Code = (component, is_single = true) => {
-        let { html, css, js, data } = component
-        const dataAsVariables = is_single ? `\
-        ${Object.entries(data)
-            .filter(field => field[0])
-            .map(field => `export let ${field[0]};`)
-            .join(`\n`)
-        }` : `\
-          ${Object.entries(data)
-                .filter(field => field[0])
-                .map(field => `let ${field[0]} = props['${field[0]}'];`)
-                .join(`\n`)
-            }`
-
-        // Move <svelte:window> to the end of the component to prevent 'can't nest' error
-        if (html.includes('<svelte:window')) {
-            let [svelteWindowTag] = html.match(/<svelte:window(.*?)\/>/);
-            html = html.replace(svelteWindowTag, '')
-            html = html + svelteWindowTag
-        }
-
-        return `
+		return `\
           <script>
             export let props;
-            ${dataAsVariables}
+            ${data_as_variables /* let some_key = props['some_key'] */}
             ${js}
           </script>
           ${css ? `<style>${css}</style>` : ``}
           ${html}`
-    }
+	}
 
-    function generate_lookup(component) {
-        if (Array.isArray(component)) { // build page (sections as components)
-            component.forEach((section, i) => {
-                const code = Component_Code(section, false)
-                component_lookup.set(`./Component_${i}.svelte`, code);
-            })
-            component_lookup.set(`./App.svelte`, `
+	function generate_lookup(component) {
+		if (Array.isArray(component)) {
+			// build page (sections as components)
+			component.forEach((section, i) => {
+				const code = Svelte_Component_Code(section, false)
+				component_lookup.set(`./Component_${i}.svelte`, code)
+			})
+			component_lookup.set(
+				`./App.svelte`,
+				`
               <script>
-                ${component.map((_, i) => `import Component_${i} from './Component_${i}.svelte';`).join('')}
+                ${component.map((_, i) => `import Component_${i} from './Component_${i}.svelte';`).join('\n')}
 
                 ${component.map((_, i) => `export let component_${i}_props`).join(`\n`)}
               </script>
-              ${component.map((section, i) => {
-                return `<Component_${i} props={component_${i}_props} /> \n`
-            }).join('')}
-          `);
-        } else { // build individual component
-            const code = Component_Code(component)
-            component_lookup.set(`./App.svelte`, code);
-        }
-    }
+              ${component
+								.map((section, i) => {
+									return `<Component_${i} props={component_${i}_props} /> \n`
+								})
+								.join('')}
+          `
+			)
+		} else {
+			// build individual component
+			const code = Svelte_Component_Code(component)
+			component_lookup.set(`./App.svelte`, code)
+		}
+	}
 
-    generate_lookup(component);
+	generate_lookup(component)
 
-    if (buildStatic) {
+	if (buildStatic) {
+		const bundle = await compile({
+			generate: 'ssr',
+			hydratable: true
+		})
 
-        const bundle = await compile({
-            generate: 'ssr',
-            hydratable: true
-        })
+		const output = (await bundle.generate({ format })).output[0].code
+		final.ssr = output
+	} else {
+		const bundle = await compile()
+		const output = (await bundle.generate({ format })).output[0].code
+		final.dom = output
+	}
 
-        const output = (await bundle.generate({ format })).output[0].code;
-        final.ssr = output
+	// If static build needs to be hydrated, include Svelte JS (or just render normal component)
+	if (hydrated) {
+		const bundle = await compile({
+			css: false,
+			hydratable: true
+		})
+		const output = (await bundle.generate({ format })).output[0].code
+		final.dom = output
+	}
 
-    } else {
-        const bundle = await compile()
-        const output = (await bundle.generate({ format })).output[0].code;
-        final.dom = output
-    }
+	async function compile(svelteOptions = {}) {
+		return await rollup({
+			input: './App.svelte',
+			plugins: [
+				commonjs,
+				{
+					name: 'repl-plugin',
+					async resolveId(importee, importer) {
+						// handle imports from 'svelte'
 
-    // If static build needs to be hydrated, include Svelte JS (or just render normal component)
-    if (hydrated) {
-        const bundle = await compile({
-            css: false,
-            hydratable: true
-        })
-        const output = (await bundle.generate({ format })).output[0].code;
-        final.dom = output
-    }
+						// import x from 'svelte'
+						if (importee === 'svelte') return `${CDN_URL}/svelte/index.mjs`
 
-    async function compile(svelteOptions = {}) {
-        return await rollup({
-            input: "./App.svelte",
-            plugins: [
-                {
-                    name: "repl-plugin",
-                    async resolveId(importee, importer) {
+						// import x from 'svelte/somewhere'
+						if (importee.startsWith('svelte/')) {
+							return `${CDN_URL}/svelte/${importee.slice(7)}/index.mjs`
+						}
 
-                        // handle imports from 'svelte'
+						// import x from './file.js' (via a 'svelte' or 'svelte/x' package)
+						if (importer && importer.startsWith(`${CDN_URL}/svelte/`)) {
+							const resolved = new URL(importee, importer).href
+							if (resolved.endsWith('.mjs')) return resolved
+							return `${resolved}/index.mjs`
+						}
 
-                        // import x from 'svelte'
-                        if (importee === "svelte") return `${CDN_URL}/svelte/index.mjs`;
+						// local repl components
+						if (component_lookup.has(importee)) return importee
 
-                        // import x from 'svelte/somewhere'
-                        if (importee.startsWith("svelte/")) {
-                            return `${CDN_URL}/svelte/${importee.slice(7)}/index.mjs`;
-                        }
+						// relative imports from a remote package
+						if (importee.startsWith('.')) {
+							return new URL(importee, importer).href
+						}
 
-                        // import x from './file.js' (via a 'svelte' or 'svelte/x' package)
-                        if (importer && importer.startsWith(`${CDN_URL}/svelte`)) {
-                            const resolved = new URL(importee, importer).href;
-                            if (resolved.endsWith(".mjs")) return resolved;
-                            return `${resolved}/index.mjs`;
-                        }
+						// importing from a URL
+						if (/^https?:/.test(importee)) return importee
 
-                        // local repl components
-                        if (component_lookup.has(importee)) return importee;
+						const match = /^((?:@[^/]+\/)?[^/]+)(\/.+)?$/.exec(importee)
+						if (!match) {
+							return console.error(`Invalid import "${importee}"`)
+						}
 
-                        // relative imports from a remote package
-                        if (importee.startsWith("."))
-                            return new URL(importee, importer).href;
+						const pkg_name = match[1]
+						const subpath = `.${match[2] ?? ''}`
 
-                        if (importee.startsWith('http')) {
-                            return importee
-                        }
+						const fetch_package_info = async () => {
+							try {
+								const pkg_url = await follow_redirects(`${CDN_URL}/${pkg_name}/package.json`)
 
-                        // get the package.json and load it into memory
-                        const pkg_url = `${CDN_URL}/${importee}/package.json`;
-                        const fetched = await fetch_package(pkg_url)
-                        const pkg = typeof (fetched) === 'object' ? JSON.parse(await fetch_package(pkg_url)) : null;
+								if (!pkg_url) throw new Error()
 
-                        // get an entry point from the pkg.json - first try svelte, then modules, then main
-                        if (pkg && (pkg.svelte || pkg.module || pkg.main)) {
-                            // use the aobove url minus `/package.json` to resolve the URL
-                            const url = pkg_url.replace(/\/package\.json$/, "");
-                            return new URL(pkg.svelte || pkg.module || pkg.main, `${url}/`)
-                                .href;
-                            } else return `https://cdn.skypack.dev/${importee}`; // use skypack as a fallback
+								const pkg_json = (await fetch_if_uncached(pkg_url))?.body
+								const pkg = JSON.parse(pkg_json ?? '""')
 
-                            return importee; // everything else
+								const pkg_url_base = pkg_url.replace(/\/package\.json$/, '')
 
-                    },
-                    async load(id) {
-                        // local repl components are stored in memory
-                        // this is our virtual filesystem
-                        if (component_lookup.has(id))
-                            return component_lookup.get(id);
+								return {
+									pkg,
+									pkg_url_base
+								}
+							} catch (_e) {
+								throw new Error(`Error fetching "${pkg_name}" from unpkg. Does the package exist?`)
+							}
+						}
 
-                        // everything else comes from a cdn
-                        return await fetch_package(id);
-                    },
-                    async transform(code, id) {
-                        // our only transform is to compile svelte components
-                        //@ts-ignore
-                        if (/.*\.svelte/.test(id)) {
-                            try {
-                                const res = await sveltePromiseWorker.postMessage({code, svelteOptions})
-                                const warnings = res.warnings
-                                    .filter(w => !w.message.startsWith(`Component has unused export`))
-                                    .filter(w => !w.message.startsWith(`A11y: <img> element should have an alt attribute`))
-                                    .filter(w => w.code !== `a11y-missing-content`)
-                                    .filter(w => !w.message.startsWith(`Unused CSS selector`)) // TODO: reinstate
-                                if (warnings[0]) {
-                                    final.error = warnings[0].message
-                                    return ''
-                                } else {
-                                    return res.code;
-                                }
-                            } catch (e) {
-                                console.log({ e, code })
-                                final.error = e.toString()
-                                return ''
-                            }
-                        }
-                    },
-                },
-            ],
-        });
-    }
+						const { pkg, pkg_url_base } = await fetch_package_info()
 
-    return final
+						try {
+							const resolved_id = await resolve_from_pkg(pkg, subpath, pkg_url_base)
+							const final = resolved_id ? new URL(resolved_id + '', `${pkg_url_base}/`).href : pkg_url_base
+							return final
+						} catch (reason) {
+							throw new Error(`Cannot import "${importee}": ${reason}.`)
+						}
+					},
+					async load(id) {
+						// local repl components are stored in memory
+						// this is our virtual filesystem
+						if (component_lookup.has(id)) return component_lookup.get(id)
 
-})
+						// everything else comes from a cdn
+						let res = await fetch_if_uncached(id)
+						return res?.body
+					},
+					async transform(code, id) {
+						// our only transform is to compile svelte components
+						//@ts-ignore
+						if (!/.*\.svelte/.test(id)) return null
+
+						try {
+							const res = await sveltePromiseWorker.postMessage({
+								code,
+								svelteOptions
+							})
+							// TODO: reinstate warnings, pass along to UI instead of throwing
+							const warnings = res.warnings
+								.filter((w) => !w.message.startsWith(`Component has unused export`))
+								.filter((w) => !w.message.startsWith(`A11y: <img> element should have an alt attribute`))
+								.filter((w) => w.code !== `a11y-missing-content`)
+								.filter((w) => !w.message.startsWith(`Unused CSS selector`)) // TODO: reinstate
+							if (warnings[0]) {
+								final.error = warnings[0].message
+								return ''
+							} else {
+								return res.code
+							}
+						} catch (e) {
+							final.error = e.toString()
+							return ''
+						}
+					}
+				},
+				json,
+				glsl
+				// replace({
+				//   'process.env.NODE_ENV': JSON.stringify('production'),
+				// }),
+			]
+			// inlineDynamicImports: true
+		})
+	}
+
+	return final
+}
+
+/**
+ * @param {string} url
+ */
+async function follow_redirects(url) {
+	const res = await fetch_if_uncached(url)
+	return res?.url
+}
+
+/** @type {Map<string, Promise<{ url: string; body: string; }>>} */
+const FETCH_CACHE = new Map()
+
+/**
+ * @param {string} url
+ */
+async function fetch_if_uncached(url) {
+	if (FETCH_CACHE.has(url)) {
+		return FETCH_CACHE.get(url)
+	}
+
+	// cache bust for files in local dev
+	if (url.includes('localhost')) {
+		url = url + '?t=' + Date.now()
+	}
+
+	// fetch the file as is, then try adding .js and /index.js
+	const promise = fetchWithFallbacks(url, ['', '.js', '/index.js'])
+	if (promise) FETCH_CACHE.set(url, promise)
+
+	return promise
+
+	async function fetchWithFallbacks(url, suffixes) {
+		for (const suffix of suffixes) {
+			const full_url = url + suffix
+
+			try {
+				const response = await fetch(full_url)
+
+				if (!response.ok || response.headers.get('content-type')?.includes('text/html')) {
+					throw new Error(`Fetch failed for URL: ${full_url}` + (await response.text()))
+				}
+
+				return {
+					url,
+					body: await response.text()
+				}
+			} catch (error) {
+				console.warn(`Failed to fetch from ${full_url}, trying next fallback.`)
+			}
+		}
+
+		console.error('All fetch attempts failed for' + url) // If all fallbacks fail
+	}
+}
+
+/**
+ *
+ * @param {Record<string, unknown>} pkg
+ * @param {string} subpath
+ * @param {string} pkg_url_base
+ */
+async function resolve_from_pkg(pkg, subpath, pkg_url_base) {
+	// match legacy Rollup logic — pkg.svelte takes priority over pkg.exports
+	if (typeof pkg.svelte === 'string' && subpath === '.') {
+		return pkg.svelte
+	}
+
+	if (pkg.jsdelivr && subpath === '.') {
+		return pkg.jsdelivr
+	}
+
+	// modern
+	if (pkg.exports) {
+		try {
+			const [resolved] =
+				resolve.exports(pkg, subpath, {
+					browser: true,
+					conditions: ['svelte', 'production']
+				}) ?? []
+
+			return resolved
+		} catch {
+			//   throw `no matched export path was found in "${pkg_name}/package.json"`
+			throw `no matched export path was found in "pkg_name/package.json"`
+		}
+	}
+
+	// legacy
+	if (subpath === '.') {
+		let resolved_id = resolve.legacy(pkg, {
+			fields: ['browser', 'module', 'main']
+		})
+
+		if (typeof resolved_id === 'object' && !Array.isArray(resolved_id)) {
+			const subpath = resolved_id['.']
+			if (subpath === false) return 'data:text/javascript,export {}'
+
+			resolved_id =
+				subpath ??
+				resolve.legacy(pkg, {
+					fields: ['module', 'main']
+				})
+		}
+
+		if (!resolved_id) {
+			// last ditch — try to match index.js/index.mjs
+			for (const index_file of ['/index.mjs', '/index.js', '.js']) {
+				try {
+					const indexUrl = new URL(index_file, `${pkg_url_base}`).href
+					return (await follow_redirects(indexUrl)) ?? ''
+				} catch {
+					// maybe the next option will be successful
+				}
+			}
+
+			//   throw `could not find entry point in "${pkg_name}/package.json"`
+			throw `could not find entry point in "pkg_name/package.json"`
+		}
+
+		return resolved_id
+	}
+
+	if (typeof pkg.browser === 'object') {
+		// this will either return `pkg.browser[subpath]` or `subpath`
+		return resolve.legacy(pkg, {
+			browser: subpath
+		})
+	}
+
+	return subpath
+}
--- src/compiler/workers/svelte.worker.js
+++ src/compiler/workers/svelte.worker.js
@@ -1,8 +1,10 @@
 import registerPromiseWorker from 'promise-worker/register'
-import {compile as svelte_compile} from '../lib/svelte-compiler.min.js'
+import { compile as svelte_compile } from '../lib/svelte-compiler.min.js'
 
 registerPromiseWorker(async function ({ code, svelteOptions }) {
-    const res = svelte_compile(code, svelteOptions)
-    // need to turn response into json string to avoid error in promise-worker
-    return { code: res?.js?.code, warnings: res.warnings.map(w => ({ message: w.message, code: w.code })) }
+  const res = svelte_compile(code, svelteOptions)
+  return {
+    code: res?.js?.code,
+    warnings: res.warnings.map((w) => ({ message: w.message, code: w.code })),
+  }
 })
