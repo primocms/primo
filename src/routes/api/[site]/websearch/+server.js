520b30fd0077c043fdaeac93874f2ba9654d5af2
John Rallis <rallisf1@yahoo.gr>
2024-01-28 23:59:26 +0200

    let's try query parameter instead

--- src/routes/api/[site]/websearch/+server.js
+++ src/routes/api/[site]/websearch/+server.js
@@ -5,6 +5,7 @@ export async function GET({ url, params }) {
 
   let options = {
     range: '0,9', // from,to # https://supabase.com/docs/reference/javascript/range
+    q: ''
   }
 
   for (const p of url.searchParams) {
@@ -13,11 +14,17 @@ export async function GET({ url, params }) {
     }
   }
 
+  if (!options.q.length) {
+    return json({
+      error: "The search term cannot be empty"
+    })
+  }
+
   const [{ data: pages_data }, { count: pages_total }] = await Promise.all([
-    supabase_admin.rpc('page_search', { search_terms: decodeURI(params.terms), site_url: params.site })
+    supabase_admin.rpc('page_search', { search_terms: options.q, site_url: params.site })
       .select('id, name, url, created_at')
       .range(parseInt(options.range.split(',')[0]), parseInt(options.range.split(',')[1])),
-    supabase_admin.rpc('page_search', { search_terms: decodeURI(params.terms), site_url: params.site }, { count: 'exact', head: true })
+    supabase_admin.rpc('page_search', { search_terms: options.q, site_url: params.site }, { count: 'exact', head: true })
   ])
 
   const pages = pages_data?.map(page => ({
