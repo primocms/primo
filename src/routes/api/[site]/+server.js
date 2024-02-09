041c77b03813f4c95b11184872d3745975507f31
John Rallis <rallisf1@yahoo.gr>
2024-02-09 06:12:18 +0200

    Api additions (#375)
    * add sections select to page api, add search endpoint
    
    * couple search fixes
    
    * let's try query parameter instead
    
    * multiple keywords search fix
    
    * move '/websearch' to '?search', fix section query from root page
    
    ---------
    
    Co-authored-by: Mateo Morris <accounts@mateomorris.com>

--- src/routes/api/[site]/+server.js
+++ src/routes/api/[site]/+server.js
@@ -1,17 +1,26 @@
-import { json } from '@sveltejs/kit';
+import { json } from '@sveltejs/kit'
 import supabase_admin from '$lib/supabase/admin'
+import page_search from './page-search'
 
-export async function GET({ params }) {
+export async function GET({ url, params }) {
+  if (url.searchParams.has('search')) {
+    return await page_search(url, params.site)
+  }
 
-  const [{data:site_res},{data:page_res},{data:subpages_data, error:subpages_error},{data:sections_res}] = await Promise.all([
-    supabase_admin.from('sites').select().filter('url', 'eq', params.site).single(),
-    supabase_admin.from('pages').select('*, site!inner(url)').match({ url: 'index', 'site.url': params.site }).single(),
-    supabase_admin.from('pages').select('*, site!inner(url)').match({ 'site.url': params.site }),
-    supabase_admin.from('sections').select('*, page!inner( site!inner(url) )').match({
-      'page.site.url': params.site,
-      'page.url': 'index'
-    })
-  ])
+  const [{ data: site_res }, { data: page_res }, { data: subpages_data }, { data: sections_res }] =
+    await Promise.all([
+      supabase_admin.from('sites').select().filter('url', 'eq', params.site).single(),
+      supabase_admin
+        .from('pages')
+        .select('*, site!inner(url)')
+        .match({ url: 'index', 'site.url': params.site })
+        .single(),
+      supabase_admin.from('pages').select('*, site!inner(url)').match({ 'site.url': params.site }),
+      supabase_admin.from('sections').select('*, page!inner( site!inner(url) )').match({
+        'page.site.url': params.site,
+        'page.url': 'index',
+      }),
+    ])
 
   const site = {
     ...site_res['content']['en'],
@@ -19,8 +28,8 @@ export async function GET({ params }) {
       id: site_res.id,
       name: site_res.name,
       url: site_res.url,
-      created_at: site_res.created_at
-    }
+      created_at: site_res.created_at,
+    },
   }
 
   const page = {
@@ -31,29 +40,31 @@ export async function GET({ params }) {
       url: page_res.url,
       created_at: page_res.created_at,
       // filtering here because the query above is not filtering properly (maybe a Supabase bug)
-      subpages: subpages_data?.filter(subpage => subpage.parent === null && subpage.url !== 'index').map(subpage => ({
-        id: subpage.id,
-        name: subpage.name,
-        url: subpage.url,
-        created_at: subpage.created_at
-      }))
+      subpages: subpages_data
+        ?.filter((subpage) => subpage.parent === null && subpage.url !== 'index')
+        .map((subpage) => ({
+          id: subpage.id,
+          name: subpage.name,
+          url: subpage.url,
+          created_at: subpage.created_at,
+        })),
     },
   }
 
-
-  const sections = sections_res?.sort((a,b) => a.index - b.index).map(section => ({
-    ...section.content.en,
-    _meta: {
-      id: section.id,
-      symbol: section.symbol,
-      created_at: section.created_at
-    }
-  }))
+  const sections = sections_res
+    ?.sort((a, b) => a.index - b.index)
+    .map((section) => ({
+      ...section.content.en,
+      _meta: {
+        id: section.id,
+        symbol: section.symbol,
+        created_at: section.created_at,
+      },
+    }))
 
   return json({
     site,
     page,
-    sections
+    sections,
   })
-
 }
