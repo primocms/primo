f4f34c1dfab2c970160d716a6a7c5d86b2f6f2ca
Matthew Morris <accounts@mateomorris.com>
2026-09-21 20:56:48 -0400

    Match the page-types dialog to the updated dialog style
    Same surface, title weight and button styling as the publish dialog, so the
    dialogs read as one set.

--- src/lib/builder/views/editor/Toolbar.svelte
+++ src/lib/builder/views/editor/Toolbar.svelte
@@ -185,7 +185,7 @@
 </Dialog.Root>
 
 <Dialog.Root bind:open={editing_pages}>
-	<Dialog.Content class="z-999 max-w-[900px] h-[calc(100vh-1rem)] max-h-none flex flex-col p-4">
+	<Dialog.Content class="z-999 max-w-[900px] h-[calc(100vh-1rem)] max-h-none flex flex-col p-4 bg-[#1e1e20] border-[#343437] rounded-lg text-[#e4e4e7]">
 		<SitePages />
 	</Dialog.Content>
 </Dialog.Root>
